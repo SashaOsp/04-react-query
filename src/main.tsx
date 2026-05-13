0a4da5bb735d81789c51b5a03407632a59add322
SashaOsp <sasha.mykol@gmail.com>
2026-05-13 16:39:38 +0300

    1 more copy

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.querySelector("#root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 );
